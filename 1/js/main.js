5b8d793807f13d036b7456403acd75a778b98aeb
luyang <lythirteen@126.com>
2015-12-22 08:20:13 +0800

    12.22

--- 1/js/main.js
+++ 1/js/main.js
@@ -3,3 +3,36 @@ window.addEventListener('resize',setRem,false);
 function setRem(){
 	document.documentElement.style.fontSize = document.documentElement.clientWidth/64 + 'px';
 }
+function Page(name){
+	this.name = name;
+}
+Page.prototype = {
+	load:function(){
+		var _this = this;
+		var curPage = $('.page-area');
+		var form = $('form')
+		if(this.name){
+			curPage.css('visibility','hidden');
+			form.addClass('loading');
+			setTimeout(function(){
+				form.removeClass('loading');
+				curPage.empty();
+				curPage.load('./'+ _this.name +'.html');
+				curPage.css('visibility','visible');
+			},1500);
+		}
+		return this;
+	}
+}
+$(document).ready(function() {
+	var step1 = new Page('s2').load();
+	
+});
+
+// document.onclick = function(){
+// 	$('.page-area').load('./s1.html');
+// }
+// $(document).ready(function(){
+// 	$('.page-area').load('s1.html')
+// 	$('.page-area').css('visibility','hidden');
+// });
